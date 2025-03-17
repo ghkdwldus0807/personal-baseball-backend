f7cc62514dc4e9d26fd96f506829caff61db91a7
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-03-17 13:34:52 +0900

    Initial commit

---
+++ src/main/java/com/personal_baseball/PersonalBaseballApplication.java
@@ -0,0 +1,13 @@
+package com.personal_baseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBaseballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal_baseball/PersonalBaseballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal_baseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBaseballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
